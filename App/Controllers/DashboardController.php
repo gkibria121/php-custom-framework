7fbf6caba8f2dd02ea7361509f0e8a066463f80d
gkibria <gkibria121@gmail.com>
2025-02-14 19:02:36 +0600

    add dashboard

---
+++ App/Controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+declare(strict_types=1);
+
+
+
+namespace App\Controllers;
+
+
+
+class DashboardController
+{
+
+    public function __construct() {}
+
+    public function index()
+    {
+        echo "This is Dashboard";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use App\Controllers\DashboardController;
 use App\Controllers\HomeController;
 use Framework\Route;
 
 
 Route::get('/home', [HomeController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
